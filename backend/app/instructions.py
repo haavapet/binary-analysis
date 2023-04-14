b99c04e3b59c8a20b8f89f4face3732fd27d2631
haavapet <haavapet@stud.ntnu.no>
2023-04-14 19:38:32 +0200

    refactor instructions.py

--- backend/app/instructions.py
+++ backend/app/instructions.py
@@ -9,61 +9,41 @@ class Instruction():
         self.call_opcode = instruction & ((0xFFFFFFFFFFFFFFFF >> (64-instr_len)) << (instr_len - call_len))
         self.call_operand = instruction & (0xFFFFFFFFFFFFFFFF >> (64-instr_len+call_len))
 
-
-# split bytestream into instructions
-#TODO this is only big(?) endian
 def extract_instruction(bin, endiannes, instr_length):
+    if endiannes == "big":
+        return _extract_instruction_big_endian(bin, instr_length)
+    elif endiannes == "little":
+        return "NOT IMPLEMENTED" #_extract_instruction_little_endian(bin, instr_length)
+    else: pass # TODO throw exception?
+
+def _extract_instruction_big_endian(bin, instr_length):
     instructions = []
 
-    # change name to byte_mask
-    hash_mask_start = {
-        0: int("0b11111111", 2),
-        1: int("0b01111111", 2),
-        2: int("0b00111111", 2),
-        3: int("0b00011111", 2),
-        4: int("0b00001111", 2),
-        5: int("0b00000111", 2),
-        6: int("0b00000011", 2),
-        7: int("0b00000001", 2),
-    }
-
-    hash_mask_end = {
-        0: int("0b11111111", 2),
-        1: int("0b11111110", 2),
-        2: int("0b11111100", 2),
-        3: int("0b11111000", 2),
-        4: int("0b11110000", 2),
-        5: int("0b11100000", 2),
-        6: int("0b11000000", 2),
-        7: int("0b10000000", 2),
-    }
-
-    # TODO so far only big? endian here, but small? endian should be mostly the same, just some things switched
-    # iterate through bits
-    for i in range(0, len(bin) * 8, instr_length): 
-        # i = 0, 16, 32, 48...
-        # find start of instruction byte and offset into byte
-
+    # iterate through instructions
+    for j in range(0, len(bin) * 8, instr_length): 
         instr = 0
-        start, start_offset = divmod(i, 8)
-        end, end_offset = divmod(i + instr_length, 8)
-        if end_offset != 0: end += 1 
-        for i, e in enumerate(reversed((bin[start:end]))):
-            # negate the first bits if not part of instruction
-            # THIS IS THE LAST BYTE
-            if i == 0: e = (e & hash_mask_end[end_offset]) >> end_offset
+        start, start_offset = divmod(j, 8)
+        end, end_offset = divmod(j + instr_length, 8)
 
-            # negate the last bits if not part of instruction
-            # THIS IS THE FIRST BYTE
-            if i == end - start - 1: e = (e & hash_mask_start[start_offset]) 
+        if end_offset == 0:
+            end -= 1
+            end_offset = 8
 
-            # move bits to correct place
-            # might need to add start_index or something when shifting
-            if i != 0:
-                instr |= e << (8*i - end_offset)
-            else:
-                instr = e
-            # print(i, start, end, start_offset, end_offset, bin[start:end],hex(a), hex(instr))
+        instruction_bits = list(bin[start:end + 1])
+
+        # negate the last bits of last byte if not part of instruction
+        instruction_bits[0] &= 0xFF << ((8 - end_offset) & 0x00FF)
+        # negate the first bits of the first byte if not part of instruction
+        instruction_bits[-1] &= 0xFF >> start_offset
+
+        # shift each bit of the instruction to the correct place
+        for i, e in enumerate(reversed((instruction_bits))):
+            # align by end, f.ex if byte is 0b00101000 and end offset is 5, then correctly shifted instr is 0b00101
+            e >>= 8 - end_offset
+            # if instruction is multiple bytes we also need to set the bytes in the correct place
+            e <<= 8*i
+            instr |= e
+
         instructions.append(instr)
 
     return instructions
