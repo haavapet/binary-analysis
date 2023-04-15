e1dc454609eb7d30a762a82c2b505177fddf1c1a
haavapet <haavapet@stud.ntnu.no>
2023-04-15 17:35:36 +0200

    refactor instructions.py and add support for little endian

--- backend/app/instructions.py
+++ backend/app/instructions.py
@@ -1,3 +1,7 @@
+from functools import reduce
+from operator import ior
+
+BYTE_LENGTH = 8
 
 class Instruction():
     call_instruction = None # TODO rename this
@@ -10,20 +14,13 @@ class Instruction():
         self.call_operand = instruction & (0xFFFFFFFFFFFFFFFF >> (64-instr_len+call_len))
 
 def extract_instruction(bin, endiannes, instr_length):
-    if endiannes == "big":
-        return _extract_instruction_big_endian(bin, instr_length)
-    elif endiannes == "little":
-        return "NOT IMPLEMENTED" #_extract_instruction_little_endian(bin, instr_length)
-    else: pass # TODO throw exception?
-
-def _extract_instruction_big_endian(bin, instr_length):
     instructions = []
 
     # iterate through instructions
-    for j in range(0, len(bin) * 8, instr_length): 
-        instr = 0
-        start, start_offset = divmod(j, 8)
-        end, end_offset = divmod(j + instr_length, 8)
+    for j in range(0, len(bin) * BYTE_LENGTH, instr_length): 
+        
+        start, start_offset = divmod(j, BYTE_LENGTH)
+        end, end_offset = divmod(j + instr_length, BYTE_LENGTH)
 
         if end_offset == 0:
             end -= 1
@@ -31,19 +28,28 @@ def _extract_instruction_big_endian(bin, instr_length):
 
         instruction_bits = list(bin[start:end + 1])
 
-        # negate the last bits of last byte if not part of instruction
-        instruction_bits[0] &= 0xFF << ((8 - end_offset) & 0xFF)
+        # If overflow (i.e last instruction has fewer than instr_len bits) then pad empty bytes
+        if len(instruction_bits) != end - start + 1:
+            overflow_bits = j + instr_length - len(bin) * BYTE_LENGTH
+            instruction_bits += [0x0] * ((overflow_bits // BYTE_LENGTH) - (overflow_bits % BYTE_LENGTH == 0) + 1)
+
         # negate the first bits of the first byte if not part of instruction
-        instruction_bits[-1] &= 0xFF >> start_offset
+        instruction_bits[0] &= 0xFF >> start_offset
 
-        # shift each bit of the instruction to the correct place
-        for i, e in enumerate(reversed((instruction_bits))):
-            # align by end, f.ex if byte is 0b00101000 and end offset is 5, then correctly shifted instr is 0b00101
-            e >>= 8 - end_offset
-            # if instruction is multiple bytes we also need to set the bytes in the correct place
-            e <<= 8*i
-            instr |= e
+        # negate the last bits of last byte if not part of instruction
+        instruction_bits[-1] &= (0xFF << (BYTE_LENGTH - end_offset)) & 0xFF
 
-        instructions.append(instr)
+        # reverse bytes if endiannes is big
+        if endiannes == "big":
+            instruction_bits = list(reversed(instruction_bits))
+
+        # if instruction is multiple bytes we also need to set the bytes in the correct place
+        instruction_bits = [e << (BYTE_LENGTH * i) for i, e in enumerate(instruction_bits)]
+
+        # align by end, f.ex if byte is 0b00101000 and end offset is 5, then correctly shifted instr is 0b00101
+        instruction_bits = [e >> (BYTE_LENGTH - end_offset) for e in instruction_bits]
+
+        # bitwise or all bytes of array together into a single instruction value
+        instructions.append(reduce(ior, instruction_bits))
 
     return instructions
